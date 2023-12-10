2de681cdeb45a7c6bb453c1a126fddb12fa41ede
Mohamed Gouda <goda61@gmail.com>
2023-12-10 23:22:01 +0200

    Show review from view

--- products/views.py
+++ products/views.py
@@ -8,5 +8,11 @@ from django.views.generic import ListView , DetailView
 class ProductList (ListView) :
     model = Product
 
+
 class ProductDetail (DetailView):
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = Review.objects.filter(product = self.get_object())
+        return context
