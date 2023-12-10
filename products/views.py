39ac966ce02c1bbfe243212cc98376d5741997bf
Mohamed Gouda <goda61@gmail.com>
2023-12-11 00:25:06 +0200

    Add Brand list

--- products/views.py
+++ products/views.py
@@ -18,3 +18,10 @@ class ProductDetail (DetailView):
         context["images"] = ProductImage.objects.filter(product = self.get_object())
         context["related"] = Product.objects.filter(brand = self.get_object().brand)
         return context
+
+
+class BrandList (ListView) :
+    model = Brand
+
+class BrandDetail (DetailView):
+    pass
