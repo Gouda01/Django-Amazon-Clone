3007a966694b4de532e9c7743b1444bf2e5d2520
Mohamed Gouda <goda61@gmail.com>
2023-12-11 21:43:53 +0200

    Add brand detail

--- products/views.py
+++ products/views.py
@@ -23,5 +23,20 @@ class ProductDetail (DetailView):
 class BrandList (ListView) :
     model = Brand
 
-class BrandDetail (DetailView):
-    pass
+class BrandDetail (ListView):
+    model = Product
+    template_name = 'products/brand_detail.html'
+
+    # Return Products of the brand
+    def get_queryset(self):
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        queryset = super().get_queryset().filter(brand=brand)
+        return queryset
+    
+    # Return Brand Details
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["brand"] = Brand.objects.get(slug=self.kwargs['slug'])
+        return context
+    
+    
