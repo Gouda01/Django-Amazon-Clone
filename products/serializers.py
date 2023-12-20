98c608a9bed072663d483215b2383bde5ab904a0
Mohamed Gouda <goda61@gmail.com>
2023-12-20 22:38:23 +0200

    Show product review average in APIs

--- products/serializers.py
+++ products/serializers.py
@@ -5,6 +5,7 @@ from .models import Brand,Product
 class ProductListSerializer (serializers.ModelSerializer):
     brand = serializers.StringRelatedField()   # To return brand name not id
     review_count = serializers.SerializerMethodField() # To Show Review count
+    avg_rate = serializers.SerializerMethodField() # To Show Review Average
     class Meta :
         model = Product
         fields = '__all__'
@@ -14,22 +15,51 @@ class ProductListSerializer (serializers.ModelSerializer):
     def get_review_count(self,object): 
         reviews = object.review_product.all().count()
         return reviews
+
+    # To Show Review Average
+    def get_avg_rate(self,object): 
+        reviews = object.review_product.all()
+        total = 0
+
+        for item in reviews :
+            total += item.rate
+        if len(reviews) > 0 :
+            avg = total / len(reviews)
+        else:
+            avg = 0
+        return avg
 
 
 
 class ProductDetailSerializer (serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
     review_count = serializers.SerializerMethodField() # To Show Review count
+    avg_rate = serializers.SerializerMethodField() # To Show Review Average
     class Meta :
         model = Product
         fields = '__all__'
+
+    # To Show Review Average
+    def get_avg_rate(self,object): 
+        reviews = object.review_product.all()
+        total = 0
+
+        for item in reviews :
+            total += item.rate
+        if len(reviews) > 0 :
+            avg = total / len(reviews)
+        else:
+            avg = 0
+        return avg
+
+
 
     # To Show Review count
     def get_review_count(self,object): 
         reviews = object.review_product.all().count()
         return reviews
     
-    
+
 
 class BrandListSerializer (serializers.ModelSerializer):
     class Meta :
