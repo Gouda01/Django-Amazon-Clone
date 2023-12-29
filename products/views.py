f7f027dbcb59499bb6432d5a1d92a66ec9a9db55
Mohamed Gouda <goda61@gmail.com>
2023-12-30 00:24:18 +0200

    Add annotate to brand list and brand details

--- products/views.py
+++ products/views.py
@@ -124,6 +124,7 @@ class ProductDetail (DetailView):
 class BrandList (ListView) :
     model = Brand
     paginate_by = 50
+    queryset = Brand.objects.annotate(product_count=Count('product_brand'))
 
 class BrandDetail (ListView):
     model = Product
@@ -139,7 +140,7 @@ class BrandDetail (ListView):
     # Return Brand Details
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["brand"] = Brand.objects.get(slug=self.kwargs['slug'])
+        context["brand"] = Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count=Count('product_brand'))[0]
         return context
     
     
