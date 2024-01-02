fa602f9b43acaee89578145adfa389e0cf98c2b6
Mohamed Gouda <goda61@gmail.com>
2024-01-03 00:26:26 +0200

    Fix caching using redis after clear chache

--- settings/settings_context_processor.py
+++ settings/settings_context_processor.py
@@ -3,10 +3,14 @@ from django.core.cache import cache
 
 def get_settings(request):
     
+    
     #Check data in cash
     try :
         settings_data = cache.get('settings_data')
+        #cache.clear(prefix="settings_data")
+        print('new cache')
     except Exception:
+        print('new data')
         settings_data = Settings.objects.last()
         cache.set('settings_data',settings_data,60*60*24)
 
