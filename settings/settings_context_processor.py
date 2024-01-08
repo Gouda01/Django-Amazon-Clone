c91adefc3bac16dcfb78edf8889b161c6b64d100
Mohamed Gouda <goda61@gmail.com>
2024-01-08 21:55:25 +0200

    Add Home page data

--- settings/settings_context_processor.py
+++ settings/settings_context_processor.py
@@ -5,13 +5,14 @@ def get_settings(request):
     
     
     #Check data in cash
-    try :
-        settings_data = cache.get('settings_data')
-        #cache.clear(prefix="settings_data")
-        print('new cache')
-    except Exception:
-        print('new data')
-        settings_data = Settings.objects.last()
-        cache.set('settings_data',settings_data,60*60*24)
+    # try :
+    #     settings_data = cache.get('settings_data')
+    #     #cache.clear(prefix="settings_data")
+    #     print('new cache')
+    # except Exception:
+    #     print('new data')
+    #     settings_data = Settings.objects.last()
+    #     cache.set('settings_data',settings_data,60*60*24)
 
+    settings_data = Settings.objects.last()
     return {'settings_data' : settings_data}
