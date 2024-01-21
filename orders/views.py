42eda01c2c6a77faeccb5b40204ebb44c0d8f3c4
Mohamed Gouda <goda61@gmail.com>
2024-01-21 23:47:16 +0200

    Add data to checkout page

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , redirect
 
 from .models import Order,OrderDetail , Cart , CartDetail , Coupon
 from products.models import Product
+from settings.models import DeliveryFee
 
 
 # Create your views here.
@@ -12,8 +13,23 @@ def order_list(request):
 
 
 def checkout(request):
-    
-    return render(request,'orders/checkout.html',{})
+
+    cart , created = Cart.objects.get_or_create(user=request.user , status='Inprogress')
+    cart= Cart.objects.get(user=request.user,status='Inprogress')
+    cart_detail = CartDetail.objects.filter(cart=cart)
+    delivery_fee = DeliveryFee.objects.last().fee
+
+    sub_total = cart.cart_total
+    discount = 0
+    total = sub_total + delivery_fee
+
+    return render(request,'orders/checkout.html',{
+        'cart_detail' : cart_detail,
+        'delivery_fee': delivery_fee,
+        'sub_total': sub_total,
+        'discount': discount,
+        'total': total,
+    })
 
 
 def add_to_cart(request):
